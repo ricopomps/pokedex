de2e834b37c0e0c375a24bf7ca5403dd97f3903b
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-14 00:03:49 -0300

    initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import Head from "next/head";
+import type { AppProps } from "next/app";
+import { Anton } from "next/font/google";
+import { Container } from "react-bootstrap";
+
+const anton = Anton({ subsets: ["latin"], weight: ["400"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Pokedex</title>
+        <meta name="description" content="Pokedex app" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className={anton.className}>
+        <main>
+          <Container className="py-4">
+            <Component {...pageProps} />
+          </Container>
+        </main>
+      </div>
+    </>
+  );
 }

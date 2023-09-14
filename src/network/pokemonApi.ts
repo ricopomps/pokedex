1cce830df9b2d9afd2218f90bc289bf42568a8e2
Ricardo Pompilio <ricardopompiliodossantos@gmail.com>
2023-09-14 01:13:04 -0300

    feat: Removed delay function

--- src/network/pokemonApi.ts
+++ src/network/pokemonApi.ts
@@ -2,7 +2,6 @@ import { Pokemon, PokemonPage } from "@/models/Pokemon";
 import api from "./axiosIntance";
 
 export async function getPokemon(name: string) {
-  await delay();
   const response = await api.get<Pokemon>(`/pokemon/${name}`);
   return response.data;
 }
@@ -13,11 +12,6 @@ export async function getPokemonPage(page: number) {
     params: { limit: pageSize, offset: pageSize * (page - 1) },
   });
   return response.data;
-}
-
-async function delay() {
-  const delay = Math.random() * 2000;
-  await new Promise((r) => setTimeout(r, delay));
 }
 
 export async function setNickname(pokemon: Pokemon, nickname: string) {
